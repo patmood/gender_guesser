f585e469cd0c18a59fc61547d68e0a71790ca71a
patmood <patrick.n.moody@gmail.com>
2013-10-02 16:29:42 -0700

    basic guess method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,9 @@ class WelcomeController < ApplicationController
   end
 
   def guess
-
+    @gender = BodyStat.guess_gender(params[:height].to_i, params[:weight].to_i)
   end
 
 end
+
+
--- app/models/body_stat.rb
+++ app/models/body_stat.rb
@@ -1,2 +1,13 @@
 class BodyStat < ActiveRecord::Base
+
+  def self.guess_gender(height, weight)
+    if profile = BodyStat.find_by_height(height)
+      male = (weight - profile.male_weight).abs
+      female = (weight - profile.female_weight).abs
+      male < female ? "male" : "female"
+    else
+      nil
+    end
+  end
+
 end
