627df380c0d1ef9083dd7cc24bf6611fc838d9c0
patmood <patrick.n.moody@gmail.com>
2013-10-02 12:39:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenderGuesser::Application.config.secret_key_base = 'c44b98f546aa43df61ccda8ff8608189e1abe24a9d19c06dd2f33e0ccd0d7e55c0cd6907c3905fa513c8bc388a26d9d074df4616e542600753c5a29d10b91594'
